99911603537e8a3c174567aa41538413f52ef6ed
aubin-karaha <c.karaha@alustudent.com>
2026-06-29 00:30:31 +0300

    Adding quests 5, 11, 17, 23, 29

---
+++ quests/quest_05_echoing_cave.py
@@ -0,0 +1,8 @@
+player_health = 100
+print("Starting health:", player_health)
+
+player_health = player_health - 25
+print("Health after monster attack:", player_health)
+
+player_health = player_health + 10
+print("Health after drinking potion:", player_health)
