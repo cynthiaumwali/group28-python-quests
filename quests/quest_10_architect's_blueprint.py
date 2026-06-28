d29b79c775b6ef670ffa6a16234fd236c42bd4ea
ucynthia13 <cynthiaumwali3@gmail.com>
2026-06-28 18:09:40 +0200

    add quest_10_architect's_blueprint.py

---
+++ quests/quest_10_architect's_blueprint.py
@@ -0,0 +1,5 @@
+length = float(input("Enter the value of the length: "))
+width = float(input("Enter the value of the width: "))
+area = length * width
+
+print(f"The area of the rectangle is: {area}")
