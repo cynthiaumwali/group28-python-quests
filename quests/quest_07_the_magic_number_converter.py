46a658ec0fe81c9251bd02334d95d9c0ecd6d804
David-Harold <ke.d08@yahoo.com>
2026-06-29 09:48:40 -0700

    Person 1 (David) Your first spell + The magic number converter + The maze of many choices + The countdown + The number wizard

---
+++ quests/quest_07_the_magic_number_converter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+birth_year = int(input("In what year where you born ? Enter your birth year : "))
+User_age = 2026 - birth_year
+
+print(f"Got it ! You are {User_age} years old.")
+
