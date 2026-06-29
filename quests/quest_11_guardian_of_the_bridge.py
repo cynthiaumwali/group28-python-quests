8aa5b051ff745ab7454a2ccb9533b1dbdb4c9f79
aubin-karaha <c.karaha@alustudent.com>
2026-06-29 21:38:52 +0300

    Quest 11: Guardian of the Bridge and Quest 23: Oracles Vision

---
+++ quests/quest_11_guardian_of_the_bridge.py
@@ -0,0 +1,9 @@
+#Quest 11
+player_age = input("How old are you? ")
+
+player_age = int(player_age)
+
+if player_age >= 18:
+    print ("You are old enough to vote! ")
+else:
+    print ("You are not yet old enough to vote. ")
---
+++ quests/quest_23_oracles_vision.py
@@ -0,0 +1,10 @@
+#Quest 23
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+area_1 = calculate_area(6, 2)
+print("Area of the first rectangle is: ", area_1)
+
+area_2 = calculate_area (10, 3)
+print("Area of the second rectangle is: ", area_2)
