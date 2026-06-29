1c5329c144188b33ba31dbdcdb1dda3735742cdd
xCHLOMIx <gutabarwaa@gmail.com>
2026-06-29 19:14:22 +0200

    Remove mistake files for wrong tasks and add question_08_potion_brewer.py and make it executable

---
+++ quests/quest_08_potion_brewer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Prices
+dragon_scale_price = 10
+elf_tear_price = 3
+
+# The total cost
+print(f"""Total cost calculation
+
+Dragon Scales × 3: {dragon_scale_price * 3}
+Elf tears × 5: {elf_tear_price * 5}
+----------------------------------
+Total cost: {(dragon_scale_price * 3) + (elf_tear_price * 5)}""")
--- quests/quest_17_endless_staircase.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-counter = 0
-
-while counter <= 5:
-    print(f"Counter is at {counter}")
-    counter += 1
